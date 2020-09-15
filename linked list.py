d941b056372fca2b1d8cce6db46bacd40608b6f4
yashpurohit4009 <61495591+yashpurohit4009@users.noreply.github.com>
2020-09-15 10:32:38 +0530

    Add files via upload

---
+++ linked list.py
@@ -0,0 +1,215 @@
+class Node: 
+
+    def __init__ (self, element, next = None ):
+        self.element = element
+        self.next = next
+        self.previous = None
+    def display(self):
+        print(self.element)
+
+class LinkedList:
+        
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        
+ 
+        
+    def _len_(self):
+        return self.size
+    
+    def get_head(self):
+        return self.head
+    
+    
+    def is_empty(self):
+        return self.size == 0 
+    
+    def display(self):
+        if self.size == 0:
+            print("No element")
+            return 
+        first = self.head
+        print(first.element.element)
+        first = first.next
+        while first:
+            if type(first.element) == type(my_list.head.element):
+                print(first.element.element)
+                first = first.next
+            print(first.element)
+            first = first.next
+            
+    def reverse_display(self):
+        if self.size == 0:
+            print("No element")
+            return None
+        last = my_list.get_tail()
+        print(last.element)
+        while last.previous:
+            if type(last.previous.element) == type(my_list.head):
+                print(last.previous.element.element)
+                if last.previous == self.head:
+                    return None
+                else:
+                    last = last.previous
+            print(last.previous.element)
+            last = last.previous
+            
+    
+    
+    def add_head(self,e):
+        #temp = self.head 
+        self.head = Node(e)
+        #self.head.next = temp
+        self.size += 1 
+        
+    def get_tail(self):
+        last_object = self.head
+        while (last_object.next != None):
+            last_object = last_object.next
+        return last_object
+        
+    
+    def remove_head(self):
+        if self.is_empty():
+            print("Empty Singly linked list")
+        else:
+            print("Removing")
+            self.head = self.head.next
+            self.head.previous = None
+            self.size -= 1
+            
+    def add_tail(self,e):
+        new_value = Node(e)
+        new_value.previous = self.get_tail()
+        self.get_tail().next = new_value
+        self.size += 1
+        
+    def find_second_last_element(self):
+        #second_last_element = None
+        
+        
+        if self.size >= 2:
+            first = self.head 
+            temp_counter = self.size -2
+            while temp_counter > 0:
+                first = first.next 
+                temp_counter -= 1 
+            return first
+        
+        
+        else:
+            print("Size not sufficient")
+            
+        return None
+
+        
+        
+    def remove_tail(self):
+        if self.is_empty():
+            print("Empty Singly linked list")
+        elif self.size == 1:
+            self.head == None
+            self.size -= 1
+        else: 
+            Node = self.find_second_last_element()
+            if Node:
+                Node.next = None
+                self.size -= 1
+                
+    def get_node_at(self,index):
+        element_node = self.head
+        counter = 0
+        if index == 0:
+            return element_node.element
+        if index > self.size-1:
+            print("Index out of bound")
+            return None
+        while(counter < index):
+            element_node = element_node.next
+            counter += 1
+        return element_node
+  
+    def get_previous_node_at(self,index):
+        if index == 0:
+            print('No previous value')
+            return None
+        return my_list.get_node_at(index).previous
+                
+    def remove_between_list(self,position):
+        if position > self.size-1:
+            print("Index out of bound")
+        elif position == self.size-1:
+            self.remove_tail()
+        elif position == 0:
+            self.remove_head()
+        else:
+            prev_node = self.get_node_at(position-1)
+            next_node = self.get_node_at(position+1)
+            prev_node.next = next_node
+            next_node.previous = prev_node
+            self.size -= 1
+            
+    def add_between_list(self,position,element):
+        element_node = Node(element)
+        if position > self.size:
+            print("Index out of bound")
+        elif position == self.size:
+            self.add_tail(element)
+        elif position == 0:
+            self.add_head(element)
+        else:
+            prev_node = self.get_node_at(position-1)
+            current_node = self.get_node_at(position)
+            prev_node.next = element_node
+            element_node.previous = prev_node
+            element_node.next = current_node
+            current_node.previous = element_node
+            self.size += 1
+        
+    def search (self,search_value):
+        index = 0 
+        while (index < self.size):
+            value = self.get_node_at(index)
+            if type(value.element) == type(my_list.head):
+                print("Searching at " + str(index) + " and value is " + str(value.element.element))
+            else:
+                print("Searching at " + str(index) + " and value is " + str(value.element))
+            if value.element == search_value:
+                print("Found value at " + str(index) + " location")
+                return True
+            index += 1
+        print("Not Found")
+        return False
+    
+    def merge(self,linkedlist_value):
+        if self.size > 0:
+            last_node = self.get_node_at(self.size-1)
+            last_node.next = linkedlist_value.head
+            linkedlist_value.head.previous = last_node
+            self.size = self.size + linkedlist_value.size
+            
+        else:
+            self.head = linkedlist_value.head
+            self.size = linkedlist_value.size
+            
+l1 = Node('Element 1')
+my_list = LinkedList()
+my_list.add_head(l1)
+my_list.add_tail('Element 2')
+my_list.add_tail('Element 3')
+my_list.add_tail('Element 4')
+my_list.get_head().element.element
+my_list.add_between_list(2,'Element between')
+my_list.remove_between_list(2)
+
+my_list2 = LinkedList()
+l2 = Node('Element 5')
+my_list2.add_head(l2)
+my_list2.add_tail('Element 6')
+my_list2.add_tail('Element 7')
+my_list2.add_tail('Element 8')
+my_list.merge(my_list2)
+my_list.get_previous_node_at(3).element
+my_list.reverse_display()
+my_list.search('Element 6')
